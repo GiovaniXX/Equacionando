78bb59cc06320271e712391c7dbba31b39112f74
GiovaniXX <giovani_chaves@hotmail.com>
2023-04-01 09:19:09 -0300

    ...

--- test/classes/JTextPaneTeste.java
+++ test/classes/JTextPaneTeste.java
@@ -6,12 +6,16 @@ import static org.hamcrest.Matchers.containsString;
 
 public class JTextPaneTeste {
 
+    private static String testResolverEquacaoComDecimal(double a, double b) {
+        throw new UnsupportedOperationException("");
+    }
+
     @Test
     public void testResolverEquacaoComDecimalTest() {
         double a = 3.5;
         double b = 1.5;
         double resultadoEsperado = -b / a;
-        //String resultado = JTextPaneTeste.testResolverEquacaoComDecimal(a, b);
-        //assertThat(resultado, containsString("Portanto, a solução da equação é x = " + resultadoEsperado));
+        String resultado = JTextPaneTeste.testResolverEquacaoComDecimal(a, b);
+        assertThat(resultado, containsString("Portanto, a solução da equação é x = " + resultadoEsperado));
     }
 }
